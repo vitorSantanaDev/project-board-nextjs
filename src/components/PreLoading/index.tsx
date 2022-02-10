9365d84fc9719c72f1fb27e98b1af2ba28279d21
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-10 10:18:24 -0300

    🎨 Created component PreLoading

---
+++ src/components/PreLoading/index.tsx
@@ -0,0 +1,22 @@
+import * as S from './styles'
+
+const PreLoading = () => {
+  return (
+    <S.Wrapper>
+      <S.Logo
+        src="/images/logo.svg"
+        alt="Uma logo com uma letra 'B' e um círculo amarelo ao lado"
+      />
+      <S.Title>A simple Project Board in NextJS</S.Title>
+      <S.Description>
+        TypeScript, ReactJS, NextJS and Styled components
+      </S.Description>
+      <S.Illustration
+        src="/images/hero-illustration.svg"
+        alt="Um usuário de tecnologia, de frente pra um notebook."
+      />
+    </S.Wrapper>
+  )
+}
+
+export default PreLoading
