aa61a6e54d2d0434a0badf46b454f2a8814026d0
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-11 00:03:13 -0300

    💄 Creating style of component SingInButton

---
+++ src/components/SigInButton/styles.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+
+export const ButtonWrapper = styled.button`
+  height: 2.5rem;
+  border-radius: 3rem;
+  cursor: pointer;
+
+  padding: 0 1.5rem;
+  margin-left: auto;
+
+  color: #fff;
+  background-color: transparent;
+
+  font-weight: bold;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  transition: ease-in-out 0.3s;
+
+  img {
+    width: 45px;
+    height: 45px;
+    margin-right: 1.2rem;
+  }
+
+  svg {
+    width: 30px;
+    height: 30px;
+
+    &:first-child {
+      margin-right: 1rem;
+    }
+  }
+
+  svg.logoutIcon {
+    margin-left: 1.2rem;
+  }
+
+  &:hover {
+    opacity: 0.8;
+  }
+`
