40adc37570e8533ad862e3645f909eb83a101afd
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-12 11:03:35 -0300

    💄 Created Home page styles

---
+++ src/styles/pages/HomeStyles.ts
@@ -0,0 +1,71 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  max-width: 1120px;
+
+  margin: 0 auto;
+  padding: 0 2rem;
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+`
+
+export const Ilustration = styled.img`
+  margin-top: 4rem;
+`
+
+export const SectionHero = styled.section`
+  max-width: 700px;
+`
+
+export const Title = styled.h1`
+  color: #141a29;
+  font-size: 2.8rem;
+  font-weight: bold;
+  text-align: center;
+
+  margin-top: 2rem;
+`
+
+export const Description = styled.h3`
+  margin-top: 2rem;
+
+  color: #141a29;
+  font-size: 2rem;
+  text-align: center;
+
+  span {
+    color: #04dd75;
+    font-weight: bold;
+  }
+`
+
+export const ListOfSupporters = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  margin: 2.5rem 0 2.5rem 0;
+
+  img {
+    width: 65px;
+    height: 65px;
+    cursor: pointer;
+
+    border-radius: 50%;
+    border: 3px solid #141a29;
+
+    transition: ease-in-out 0.3s;
+
+    + img {
+      margin-left: 1.5rem;
+    }
+
+    &:hover {
+      opacity: 0.8;
+      transform: scale(1.05);
+    }
+  }
+`
