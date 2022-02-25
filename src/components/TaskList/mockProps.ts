3858292ce99ba8dd9404d414058484f9f431747a
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-24 22:09:14 -0300

    perf: :zap: creating a mock of props the component TaskList

---
+++ src/components/TaskList/mockProps.ts
@@ -0,0 +1,42 @@
+export const tasksProps = [
+  {
+    id: '3896433',
+    userId: '8458',
+    name: 'Name the User',
+    tarefa: 'Name Task',
+    created: new Date(),
+    createdFormated: 'Date of Created'
+  },
+  {
+    id: '39033',
+    userId: '8456458',
+    name: 'Name the User',
+    tarefa: 'Name Task',
+    created: new Date(),
+    createdFormated: 'Date of Created'
+  },
+  {
+    id: '38933',
+    userId: '865658',
+    name: 'Name the User',
+    tarefa: 'Name Task',
+    created: new Date(),
+    createdFormated: 'Date of Created'
+  },
+  {
+    id: '3366553',
+    userId: '85654658',
+    name: 'Name the User',
+    tarefa: 'Name Task',
+    created: new Date(),
+    createdFormated: 'Date of Created'
+  },
+  {
+    id: '337763',
+    userId: '88',
+    name: 'Name the User',
+    tarefa: 'Name Task',
+    created: new Date(),
+    createdFormated: 'Date of Created'
+  }
+]
