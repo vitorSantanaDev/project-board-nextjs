56c6af73b1a8fcafb8e21a82e173b723399388dc
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-03-08 22:38:37 -0300

    feat: :sparkles: making the last donation date dynamic

--- src/components/ThanksForSupporting/index.tsx
+++ src/components/ThanksForSupporting/index.tsx
@@ -1,13 +1,27 @@
 import { FiClock } from 'react-icons/fi'
+import { formatDistance } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
 import * as S from './styles'
 
-const ThanksForSupporting: React.FC = () => {
+interface ThanksForSupportingProps {
+  lastDonateUser: string | Date
+}
+
+const ThanksForSupporting: React.FC<ThanksForSupportingProps> = ({
+  lastDonateUser
+}) => {
   return (
     <S.Wrapper>
       <S.Message>Obrigado por apoiar esse projeto.</S.Message>
       <S.TimeOfLastDonation>
         <FiClock size={28} color="#FFF" />
-        <time>Última doação foi a 3 dias.</time>
+        <time>
+          Última doação foi a{' '}
+          {formatDistance(new Date(lastDonateUser), new Date(), {
+            locale: ptBR
+          })}
+        </time>
       </S.TimeOfLastDonation>
     </S.Wrapper>
   )
