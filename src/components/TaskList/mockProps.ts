ee47727f51c571b9f2ea953f8ddb7b35ddf2e1a7
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-03-11 00:08:46 -0300

    test: :white_check_mark: created TaskList component tests

--- src/components/TaskList/mockProps.ts
+++ src/components/TaskList/mockProps.ts
@@ -1,42 +1,47 @@
-export const tasksProps = [
+export const listTasks = [
   {
     id: '3896433',
     userId: '8458',
-    name: 'Name the User',
-    tarefa: 'Name Task',
+    name: 'Name the User 1',
+    tarefa: 'Name Task 1',
     created: new Date(),
     createdFormated: 'Date of Created'
   },
   {
     id: '39033',
     userId: '8456458',
-    name: 'Name the User',
-    tarefa: 'Name Task',
+    name: 'Name the User 2',
+    tarefa: 'Name Task 2',
     created: new Date(),
     createdFormated: 'Date of Created'
   },
   {
     id: '38933',
     userId: '865658',
-    name: 'Name the User',
-    tarefa: 'Name Task',
+    name: 'Name the User 3',
+    tarefa: 'Name Task 3',
     created: new Date(),
     createdFormated: 'Date of Created'
   },
   {
     id: '3366553',
     userId: '85654658',
-    name: 'Name the User',
-    tarefa: 'Name Task',
+    name: 'Name the User 4',
+    tarefa: 'Name Task 4',
     created: new Date(),
     createdFormated: 'Date of Created'
   },
   {
     id: '337763',
     userId: '88',
-    name: 'Name the User',
-    tarefa: 'Name Task',
+    name: 'Name the User 5',
+    tarefa: 'Name Task 5',
     created: new Date(),
     createdFormated: 'Date of Created'
   }
 ]
+
+export const taskListProps = {
+  handleClickButtonDeleteTask: jest.fn(),
+  handleClickButtonEditTask: jest.fn()
+}
